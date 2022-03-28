75f49478f2118583e42b1f9b76eb831c61ab078f
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2022-03-29 01:54:21 +0900

    main

--- practice/auto_download_img/download_image.py
+++ practice/auto_download_img/download_image.py
@@ -4,13 +4,13 @@ from selenium import webdriver
 
 max_cnt = 20 # 최대 받는 갯수 설정
 keyword = "wallpaper" #여러가지 다른 소스를 받을수도 있기 때문에 키워드를지정한다.
-url = f'https://www.pexels.com/ko-kr/search/{keyword}/'
+urls = f'https://www.pexels.com/ko-kr/search/{keyword}/'
 
 browser = webdriver.Chrome()
 browser.maximize_window()
-browser.get(url) #위 설정한 url로 접속
+browser.get(urls) #위 설정한 url로 접속
 
-photo_items = browser.find_elements_by_class_name('photo-item__img') 
+photo_items = browser.find_elements_by_class_name(('photo-item__img')) 
 #구글에서 이미지 - 검사 를 통해 클래스를 확인한 후 클래스명을 넣어줘야함
 img_urls = [x.get_attribute('data-big-src') for x in photo_items]
 #홈페이지에서 확인한 데이터 빅 소스에서 이미지가 바로 뜨기 때문에 데이터 빅스소를 바로 넣어줌
