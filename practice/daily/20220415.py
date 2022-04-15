2dfc758de0f5358203ceb8b9d728fe2296adaefa
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2022-04-15 19:42:55 +0900

    -___-

---
+++ practice/daily/20220415.py
@@ -0,0 +1,24 @@
+#### 4월 15일 코드업 문제 풀이
+#codeup 6027 번
+
+# print('%x' %int(input()))
+
+
+# #codeup 6011 번
+
+# print(int(input()))
+# print(float(input()))
+
+
+# y, m, d = input().split('.')
+
+# print(d,m,y,sep='-')
+
+# n = int(input())
+
+# for i in range(1, n+1):
+#     if i%10 ==3 or i%10 == 6 or i%10 ==9:
+#         print("X", end ='')
+        
+#     else:
+#         print(i, end ='')
