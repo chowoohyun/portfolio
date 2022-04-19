bb508be082004d5bc3a7ccb0362d167d93644e71
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2022-04-19 19:08:11 +0900

    -ㅅ-

---
+++ practice/daily/20220419.py
@@ -0,0 +1,25 @@
+##codeup 6091 
+
+#최소 공배수 문제
+
+# a, b, c = map(int, input().split())
+
+#풀이 1
+# d= 1
+
+# while d%a != 0 or d%b != 0 or d%c != 0:
+#     d += 1
+
+# print(d)
+
+
+# ##풀이2
+# i = 1
+
+# while True :
+#     i +=1
+    
+#     if i % a == 0 and i%b ==0 and i%c == 0:
+#         print(i)
+#         break
+
