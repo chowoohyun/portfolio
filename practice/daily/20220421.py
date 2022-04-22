7ce2bea9f124cb190b5fa14a62c7f6b1f77d37e4
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2022-04-22 18:24:31 +0900

    -_______-

--- practice/daily/20220421.py
+++ practice/daily/20220421.py
@@ -1,42 +1,42 @@
 ## codeup 6098
 ##성실한 개미
 
-m =[]
+# m =[]
 
-for  i in range(12):
-    m.append([])
-    for j in range(12):
-        m[i].append(0)
-##2차원 배열 생성
+# for  i in range(12):
+#     m.append([])
+#     for j in range(12):
+#         m[i].append(0)
+# ##2차원 배열 생성
 
-for i in range(10):
-    a = input().split()
-    for j in range(10):
-        m[i+1][j+1] = int(a[j])
-##입력 받은 값 +1 해서 넣어주기
+# for i in range(10):
+#     a = input().split()
+#     for j in range(10):
+#         m[i+1][j+1] = int(a[j])
+# ##입력 받은 값 +1 해서 넣어주기
 
 
-x, y = 2, 2
+# x, y = 2, 2
 
-while True:
-    if m[x][y] ==0:
-        m[x][y] = 9
-    elif m[x][y] ==2:
-        m[x][y]=9
-        break
+# while True:
+#     if m[x][y] ==0:
+#         m[x][y] = 9
+#     elif m[x][y] ==2:
+#         m[x][y]=9
+#         break
 
-    if(m[x][y+1]==1 and m[x+1][y] ==1 )or (x==9 and y==9):
-        break
+#     if(m[x][y+1]==1 and m[x+1][y] ==1 )or (x==9 and y==9):
+#         break
 
-    if m[x][y+1] != 1 :
-        y+=1
-    elif m[x+1][y] !=1:
-        x+=1
+#     if m[x][y+1] != 1 :
+#         y+=1
+#     elif m[x+1][y] !=1:
+#         x+=1
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(m[i][j], end=' ')
-    print()
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(m[i][j], end=' ')
+#     print()
 
 
 
