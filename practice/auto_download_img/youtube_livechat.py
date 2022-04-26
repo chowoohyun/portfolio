50b42a9c3292eceb5fea7a705fee42448c5b8dc0
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2022-04-27 03:04:42 +0900

    ㅠㅠ

--- practice/auto_download_img/youtube_livechat.py
+++ practice/auto_download_img/youtube_livechat.py
@@ -1,19 +1,25 @@
-from pytchat import LiveChat
-import pafy
-import pandas as pd
+# from pytchat import LiveChat
+# import pafy
+# import pandas as pd
 
-pafy.set_api_key('AIzaSyAB840zsA2T64CfXuzwiwiLb8LliwUKnYw')
+# pafy.set_api_key('AIzaSyAB840zsA2T64CfXuzwiwiLb8LliwUKnYw')
 
-#https://www.youtube.com/watch?v=JWreew4k_Ok
+# #https://www.youtube.com/watch?v=JWreew4k_Ok
 
-video_id = 'z5A7FrFqDWM'
+# video_id = 'z5A7FrFqDWM'
 
-v = pafy.new(video_id)
-title = v.title
-author = v.author
-published = v.published
+# v = pafy.new(video_id)
+# title = v.title
+# author = v.author
+# published = v.published
 
-print(title)
-print(author)
-print(published)
+# print(title)
+# print(author)
+# print(published)
 
+import pytchat
+
+chat = pytchat.create(video_id="JWreew4k_Ok")
+while chat.is_alive():
+    for c in chat.get().sync_items():
+        print(f"{c.datetime} [{c.author.name}]- {c.message}")
