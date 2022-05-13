f0f41426ca38bac0d48100ccc827e284bea89366
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2022-05-13 21:08:20 +0900

    22

---
+++ metaverseacademy/20220513.py
@@ -0,0 +1,60 @@
+#print(7//4)
+# print('Python is ' + 'very!' * 2 + ' useful')
+
+# s = 'Python'
+# print(s[3])
+
+# s = 'Python is very useful'
+
+# print(s[-11:10])
+
+# s = 'python'
+
+# s = s.upper()
+
+# print(s)
+
+# a = 'Python is very useful'
+
+# print(a.split())
+
+# if 15 and 30:
+#     print('true')
+
+
+# score = 60
+# test = 'su' if score >= 50 else 'test'
+
+# # print(test)
+# s = ['Python ','is ','very ','useful']
+# val = ''
+# cnt = 0
+# while cnt <= 3:
+#     val += s[cnt]
+#     cnt += 1
+# print(val)
+
+# t = [(4,2),(3,1)]
+# for (a,b) in t:
+#     print(a+b, end='')
+
+# score = [60, 40, 90, 80, 10]
+# for val in score:
+#     val += val
+#     print(val, end=' ')
+# print(val)
+
+
+# add = 0
+
+# for i in range(1, 5):
+#     add += i
+
+# print(add)
+
+# li = [1, 3, 9, 11, 13]
+# for x in li:
+#     if x >10: break
+#     print(x)
+# else:
+#     print('finish')
