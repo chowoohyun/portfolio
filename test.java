ddcc3f035af5a5b997674c7483b8d35d43de1145
chowoohyun <67001050+chowoohyun@users.noreply.github.com>
2024-04-13 21:15:16 +0900

    2024-04-13
    포트폴리오 업데이트

---
+++ test.java
@@ -0,0 +1,15 @@
+class Test{
+    int feild1;
+    String feild2;
+
+    public void method1(){
+        System.out.println("method1");
+    }
+}
+
+public class prog{
+    public static void main(String[] args){
+        Test t = new Test();
+        t.method1();
+    }
+}
